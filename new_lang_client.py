37a6664094d64de01aabd90da0ac51edd2b92ed1
adeancolon <adean.colon@upr.edu>
2019-12-10 03:08:22 -0400

    Implemented remaining client functions. Also improved server methods.

--- new_lang_client.py
+++ new_lang_client.py
@@ -1,4 +1,16 @@
 import socket
 
 def create_socket():
+    print('Creating socket')
     return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect(s):
+    s.connect()
+    print('Established connection')
+
+def receive(s):
+    s.recv(4096)
+
+def close(s):
+    s.close()
+    print('Disconnected')
--- new_lang_server.py
+++ new_lang_server.py
@@ -1,16 +1,28 @@
 import socket
 
 def create_socket():
+    print('Creating socket')
     return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 def bind(s, host, port):
     s.bind(host, port)
+    print('Binded socket')
 
 def listen(s):
     s.listen(10)
+    print('listening...')
+
 
 def accept(s):
+    print('Got connection.')
     return s.accept()
+
+def send(connection):
+    connection.send('Succesfully connected')
+    print('Response sent')
+
 
 def close(s):
     s.close()
+    print('Session terminated')
